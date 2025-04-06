3b3a7f1e755b6d47cd155df19a30184d297fc931
venkat-sujana <venkat.bvp34@gmail.com>
2025-04-06 17:39:45 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Home from "./Home";
+function App() {
+  return (
+    <div className="App">
+      <h1>This is my first react app</h1>
+      {/* Add some comments to explain what this component does. */}
+      {/* TODO: Add a link to the GitHub repo. */}
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
